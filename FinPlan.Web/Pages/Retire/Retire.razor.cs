a8168b3594172a4d49f0c37a6d659ec56e7f9a34
nandandkulkarni <84347353+nandandkulkarni@users.noreply.github.com>
2025-10-11 22:38:22 -0400

    retire move

--- FinPlan.Web/Pages/Retire/Retire.razor.cs
+++ FinPlan.Web/Pages/Retire/Retire.razor.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace FinPlan.Web.Components.Pages
+namespace FinPlan.Web.Pages.Retire
 {
     public partial class Retire : IDisposable
     {
